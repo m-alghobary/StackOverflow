3d3984ed09fc3ea8c2ca6b0476c8f86d8019e634
m-alghobary <m.alghobary90@gmail.com>
2023-09-08 21:47:22 +0300

    added the first endpoint (runs in about 45s 🤣)

---
+++ StackOverflow/Controllers/PostsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StackOverflow.Data;
+
+namespace StackOverflow.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PostsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public PostsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var posts = _context.Posts
+            .OrderByDescending(row => row.ClosedDate)
+            .Skip(0)
+            .Take(20);
+
+        return Ok(posts);
+    }
+}
